2d42fcf2d49f60e96f758cd39dfcda6bb5831fbf
jlsajfj <jlsajfj@hotmail.com>
2021-01-15 12:44:22 -0500

    clear functionality sorta works

--- main.js
+++ main.js
@@ -12,23 +12,75 @@ client.on("message", msg => {
 	if(msg.author.id === client.user.id) return
 	if(msg.mentions.users){
 		if(msg.mentions.users.keyArray().includes(client.user.id)){
-			console.log(FgYellow + `${msg.author.username} sent the message ${msg.content}` + Reset)
-			commands(msg);
+			console.log(FgYellow + `${msg.author.username} sent the message: ${msg.content}` + Reset)
+			mental(msg);
 		}
 	}
 });
 
-function commands(msg) {
+var commands = {
+	'clear': clear
+}
+
+function mental(msg) {
 	var args = msg.content.split(' ')
 	if(args.length == 1){
-		msg.channel.send('what do you want')
-			.then(message => console.log(`${FgGreen}Sent message: ${message.content}${Reset}`))
-			.catch(console.error);
+		sendSuccess(msg, 'what do you want')
+		return
+	}
+	if(args[0]!=`<@!${client.user.id}>`){
+		sendSuccess(msg,'did you tag me by mistake?')
+		return
+	}
+	if(!(args[1] in commands)){
+		sendFail(msg, 'invalid command')
+		return
+	}
+	commands[args[1]](msg, args)
+}
+
+async function clear(msg, args){
+	var cnl = msg.channel
+	if(args.length == 2){
+		deleteMessages(cnl, 99)
+		return
+	}
+	if(args.length == 3){
+		if(isNaN(args[2])){
+			return
+		}
+		var cnt = parseInt(args[2])+1
+		if(cnt < 100){
+			deleteMessages(cnl, cnt)
+			return
+		}
+		while(cnt > 0){
+			if(cnt > 100){
+				await deleteMessages(cnl, 99)
+				await new Promise(r => setTimeout(r, 1500));
+				cnt -= 100
+				continue
+			}
+			await deleteMessages(cnl, cnt)
+			cnt = 0
+		}
 		return
 	}
 }
 
-//copied from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+async function deleteMessages(cnl, count){
+	var m;
+	sendWarning(1, cnl, `Clearing ${count} messages`).then( message => {
+		m = message
+	})
+	await cnl.bulkDelete(count+1).then(messages => {
+		console.log(`${FgGreen}Bulk deleted ${messages.size-1} message(s)${Reset}`)
+		m.delete()
+		return
+	}).catch(console.error)
+}
+
+// copied from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 Reset = "\x1b[0m"
 Bright = "\x1b[1m"
 Dim = "\x1b[2m"
@@ -54,3 +106,42 @@ BgBlue = "\x1b[44m"
 BgMagenta = "\x1b[45m"
 BgCyan = "\x1b[46m"
 BgWhite = "\x1b[47m"
+
+// this isnt copied
+async function sendSuccess(recv, msg){
+	return sendColor(recv, msg, FgGreen)
+}
+
+async function sendFail(recv, msg){
+	return sendColor(recv, msg, FgRed)
+}
+
+async function sendWarning(recv, msg){
+	return sendColor(recv, msg, FgYellow)
+}
+
+async function sendWarning(flag, recv, msg){
+	return sendColor(flag, recv, msg, FgYellow)
+}
+
+async function sendColor(recv, msg, color){
+	var m
+	await recv.channel.send(msg)
+			.then(message => {
+				console.log(`${color}Sent message: ${message.content}${Reset}`)
+				m = message
+			})
+			.catch(console.error);
+	return m
+}
+
+async function sendColor(flag, recv, msg, color){
+	var m
+	await recv.send(msg)
+			.then(message => {
+				console.log(`${color}Sent message: ${message.content}${Reset}`)
+				m = message
+			})
+			.catch(console.error);
+	return m
+}
