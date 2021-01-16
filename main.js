b5c8900e803fa54afdeec5a1376fdb6b44fc06b4
jlsajfj <jlsajfj@hotmail.com>
2021-01-15 20:35:05 -0500

    moved colors outside of main package and destructured inside

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
-// copied from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-Reset = "\x1b[0m"
-Bright = "\x1b[1m"
-Dim = "\x1b[2m"
-Underscore = "\x1b[4m"
-Blink = "\x1b[5m"
-Reverse = "\x1b[7m"
-Hidden = "\x1b[8m"
+const Discord = require("discord.js")
+const config = require("./config.json")
+const replies = require("./replies.json")
+// const colors = require("./colors.js");
+// import * from './colors.js'
+// taken from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color but redone my way
 
-FgBlack = "\x1b[30m"
-FgRed = "\x1b[31m"
-FgGreen = "\x1b[32m"
-FgYellow = "\x1b[33m"
-FgBlue = "\x1b[34m"
-FgMagenta = "\x1b[35m"
-FgCyan = "\x1b[36m"
-FgWhite = "\x1b[37m"
-
-BgBlack = "\x1b[40m"
-BgRed = "\x1b[41m"
-BgGreen = "\x1b[42m"
-BgYellow = "\x1b[43m"
-BgBlue = "\x1b[44m"
-BgMagenta = "\x1b[45m"
-BgCyan = "\x1b[46m"
-BgWhite = "\x1b[47m"
-// end of copied stuff
-
-const Discord = require("discord.js");
-const config = require("./config.json");
-const replies = require("./replies.json");
+const { Reset, Bright, Dim, Underscore, Blink, Reverse, Hidden, FgBlack, FgRed, FgGreen, FgYellow, FgBlue, FgMagenta, FgCyan, FgWhite, BgBlack, BgRed, BgGreen, BgYellow, BgBlue, BgMagenta, BgCyan, BgWhite } = require("./colors.js");
 
 const client = new Discord.Client();
 client.login(config.token);
