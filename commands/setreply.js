91fead48c00ec5842f5d9075c32e10a3c76d6e4d
jlsajfj <jlsajfj@hotmail.com>
2021-03-03 23:05:58 -0500

    changed setreply to be not async

--- commands/setreply.js
+++ commands/setreply.js
@@ -2,16 +2,19 @@ const Send = require("../send.js")
 const Log = require("../logging.js")
 const { readFile, writeFile } = require('fs')
 
-async function setreply(msg, args){
-    await readFile('./auto_replies.json', (err, data) => {
-        if (err) throw err;
-        var auto_replies = JSON.parse(data);
-        auto_replies[args[2]]=args[3]
-        writeFile('./auto_replies.json', JSON.stringify(auto_replies, null, 4), (err, data) => {
+function setreply(msg, args){
+    return new Promise( done => {
+        readFile('./auto_replies.json', (err, data) => {
             if (err) throw err;
-            Send.success(msg, `Set ${args[2]} = ${args[3]}`)
-        })
-    });
+            var auto_replies = JSON.parse(data);
+            auto_replies[args[2]]=args[3]
+            writeFile('./auto_replies.json', JSON.stringify(auto_replies, null, 4), (err, data) => {
+                if (err) throw err
+                Send.success(msg, `Set ${args[2]} = ${args[3]}`)
+                done(`Set ${args[2]} = ${args[3]}`)
+            })
+        });
+    })
 }
 
 module.exports = setreply
