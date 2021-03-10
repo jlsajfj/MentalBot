ae0107f820e70f29ee6d305f228c37666b1f8f4d
jlsajfj <jlsajfj@hotmail.com>
2021-03-10 10:23:39 -0500

    updated send method to be more centralized

--- send.js
+++ send.js
@@ -2,74 +2,27 @@ const { TextChannel, GuildMember } = require("discord.js")
 const Log = require("./logging.js")
 
 function sendSuccess(recv, msg){
-    return new Promise( (done, error) => {
-        if(recv instanceof TextChannel || recv instanceof GuildMember){
-            recv.send(msg)
-                    .then(message => {
-                        Log.success(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-        else{
-            recv.channel.send(msg)
-                    .then(message => {
-                        Log.success(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-    });
+    return sendMain(recv, msg, Log.success)
 }
 
 function sendFail(recv, msg){
-    return new Promise( (done, error) => {
-        if(recv instanceof TextChannel){
-            recv.send(msg)
-                    .then(message => {
-                        Log.fail(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-        else{
-            recv.channel.send(msg)
-                    .then(message => {
-                        Log.fail(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-    });
+    return sendMain(recv, msg, Log.fail)
 }
 
 function sendInfo(recv, msg){
-    return new Promise( (done, error) => {
-        if(recv instanceof TextChannel){
-            recv.send(msg)
-                    .then(message => {
-                        Log.info(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-        else{
-            recv.channel.send(msg)
-                    .then(message => {
-                        Log.info(`Sent message: ${message.content}`)
-                        done(message);
-                    })
-                    .catch(error);
-        }
-    });
+    return sendMain(recv, msg, Log.info)
 }
 
 function sendColor(recv, msg, color){
+    return sendMain(recv, msg, Log.color, color)
+}
+
+function sendMain(recv, msg, log_func, color){
     return new Promise( (done, error) => {
         if(recv instanceof TextChannel){
             recv.send(msg)
                     .then(message => {
-                        Log.color(`Sent message: ${message.content}`, color)
+                        log_func(`Sent message: ${message.content}`, color)
                         done(message);
                     })
                     .catch(error);
@@ -77,15 +30,12 @@ function sendColor(recv, msg, color){
         else{
             recv.channel.send(msg)
                     .then(message => {
-                        Log.color(`Sent message: ${message.content}`, color)
+                        log_func(`Sent message: ${message.content}`, color)
                         done(message);
                     })
                     .catch(error);
         }
     });
-}
-
-function send(recv, msg, type, color){
 }
 
 module.exports = {
