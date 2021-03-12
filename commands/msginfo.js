590eef1220cb83989fd34a8d194cbc2bb2cb76b2
jlsajfj <jlsajfj@hotmail.com>
2021-03-12 09:50:55 -0500

    basic info now complete

--- commands/msginfo.js
+++ commands/msginfo.js
@@ -1,10 +1,35 @@
 const Send = require("../send.js")
 const Log = require("../logging.js")
+const { MessageEmbed } = require('discord.js')
 
-
+function msginfo(msg, args){
+    var cnl = msg.channel;
+    if (msg.reference){
+        var refID = msg.reference.messageID
+        cnl.messages.fetch(refID).then( found_message => {
+            var stamp = `Sent at: ${new Date(found_message.createdTimestamp).toLocaleString()}`
+            if(found_message.editedTimestamp){
+                stamp += `\nEdited at: ${new Date(found_message.editedTimestamp).toLocaleString()}`
+            }
+            
+            const found_author = found_message.author
+            const author_avatar = `https://cdn.discordapp.com/avatars/${found_author.id}/${found_author.avatar}.png`
+            
+            const found_channel = found_message.channel
+            const message_link = `https://discord.com/channels/${found_channel.guild.id}/${found_channel.id}/${found_message.id}`
+            
+            const info_embed = new MessageEmbed()
+                .setColor('#FFECAC')
+                .setTitle('Message Info')
+                .setAuthor(found_author.username, author_avatar)
+                .setDescription(`Content: \`${found_message.content}\``)
+                .setFooter(stamp)
+            Send.success(msg, info_embed)
+        })
+    }
+}
 
 module.exports = {
-    name: 'msginfo',
-    desc: 'Pin a message',
-    func: () => {}
+    desc: 'Get the details about a message',
+    func: msginfo
 }
