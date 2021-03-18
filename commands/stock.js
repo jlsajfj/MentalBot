21830570ddb9d607b6cf402dba80eebd59c49698
jlsajfj <jlsajfj@hotmail.com>
2021-03-18 13:02:46 -0400

    finally got stock chart to work. there is a discord.js bug with editing message

--- commands/stock.js
+++ commands/stock.js
@@ -1,14 +1,41 @@
 const Send = require("../send.js")
 const Log = require("../logging.js")
 const { MessageEmbed } = require('discord.js')
-const { spawn } = require('child_process')
+const { execFile } = require('child_process')
+const { unlink } = require('fs')
+const { join } = require("path")
 
 function stock_chart(msg, args){
-    if(args.length < 2) return Send.fail('Not enough args')
+    if(args.length < 3) return Send.fail('Not enough args')
+    Send.info(msg, `Attempting to fetch chart for ${args[2].toUpperCase()}`).then( new_msg => {
+        // const pwd = spawn('pwd').stdout.on('data', Log.info)
+        const stock_find_process = execFile('./commands/stock-chart.py', [args[2], 'default'])
+        
+        stock_find_process.stdout.on('data', data => {
+            var trimmed_data = data.trim()
+            var path = join(__dirname, `../${trimmed_data}`)
+            Log.info(`File ${trimmed_data} generated at ${path}`)
+            
+            const stock_embed = new MessageEmbed()
+                .setColor('#FFECAC')
+                .setTitle(`Stock Chart for ${args[2].toUpperCase()}`)
+                .attachFiles([path])
+                .setImage(`attachment://${trimmed_data}`)
+                .setFooter("Retrieved")
+                .setTimestamp()
+            Send.success(msg, stock_embed).then( () => {
+                new_msg.delete()
+                unlink(path, (err) => {
+                    if (err) throw err;
+                    Log.success('Temp file removed')
+                })
+            })
+        })
+    })
 }
 
 module.exports = {
-    name: 'stock'
+    name: 'stock',
     desc: 'Get stock chart',
     func: stock_chart
 }
