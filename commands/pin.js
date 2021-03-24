41e53b19b0d61915314fb0ab120c0d221939f095
jlsajfj <jlsajfj@hotmail.com>
2021-03-24 14:34:56 -0400

    #002

--- commands/pin.js
+++ commands/pin.js
@@ -2,25 +2,24 @@ const Send = require("../send.js")
 const Log = require("../logging.js")
 
 function pin(msg, args){
-    var reas = "MentalBot was told to"
-    if(args.length > 2){
-        reas = args[2]
-        for (var i = 3; i < args.length; i++)
-            reas += ' ' + args[i]
-    }
     return new Promise( (done, error) => {
+        var _reason = "MentalBot was told to"
+        if(args.length > 2){
+            _reason = args[2]
+            for (var i = 3; i < args.length; i++)
+                _reason += ' ' + args[i]
+        }
         var cnl = msg.channel;
         if (msg.reference){
             var refID = msg.reference.messageID;
             cnl.messages.fetch(refID).then(found => {
-                found.pin({ reason: reas }).then(result => {
-                    Log.success(`Pin success for message "${found.content}", with reason "${reas}"`)
+                found.pin({ reason: _reason }).then(result => {
+                    Log.success(`Pin success for message "${found.content}", with reason "${_reason}"`)
                     done(result)
                 }).catch(error)
             }).catch(error)
         } else {
-            Send.fail(`No referenced message`)
-            error(new Error('No referenced message'))
+            error(`No referenced message`)
         }
     })
 }
