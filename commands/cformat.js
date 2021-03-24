19c87c02b1bfd5aae01dad40a77f25bdbbe637e4
jlsajfj <jlsajfj@hotmail.com>
2021-03-24 14:23:45 -0400

    same as last commit: messed up formatting

--- commands/cformat.js
+++ commands/cformat.js
@@ -6,38 +6,40 @@ const { MessageEmbed } = require('discord.js')
 
 function code_format(msg, args){
     if(args.length < 2) return Send.fail('Not enough args')
-    var cnl = msg.channel;
-    if (msg.reference){
-        var refID = msg.reference.messageID
-        cnl.messages.fetch(refID).then( found_message => {
-            var found_content = found_message.content
-            while(found_content.endsWith('`')){
-                found_content = found_content.slice(0, -1)
-            }
-            while(found_content.startsWith('`')){
-                found_content = found_content.slice(1)
-            }
-            var lang = ''
-            if(args[2]){
-                lang = args[2]
-            } else {
-                /*?=TODO: implement code language detection
-                Log.info('Attempting to detect code language')
-                const temp_file = Math.random().toString(36).substring(7)
-                writeFile(`./commands/${temp_file}`, found_content, (err, data) => {
-                    if (err) throw err
-                    // const lang_detect = spawn('python', ['cformat_detect.py',
-                })*/
-            }
-            const code_embed = new MessageEmbed()
-                .setColor('#FFECAC')
-                .setTitle('Formatted Code')
-                .setDescription(`\`\`\`${lang}\n${found_content}\`\`\``)
-            return Send.success(msg, code_embed)
-        })
-    } else {
-        return Send.fail('A replied message is required')
-    }
+    return new Promise( (done, error) => {
+        var cnl = msg.channel;
+        if (msg.reference){
+            var refID = msg.reference.messageID
+            cnl.messages.fetch(refID).then( found_message => {
+                var found_content = found_message.content
+                while(found_content.endsWith('`')){
+                    found_content = found_content.slice(0, -1)
+                }
+                while(found_content.startsWith('`')){
+                    found_content = found_content.slice(1)
+                }
+                var lang = ''
+                if(args[2]){
+                    lang = args[2]
+                } else {
+                    /*?=TODO: implement code language detection
+                    Log.info('Attempting to detect code language')
+                    const temp_file = Math.random().toString(36).substring(7)
+                    writeFile(`./commands/${temp_file}`, found_content, (err, data) => {
+                        if (err) throw err
+                        // const lang_detect = spawn('python', ['cformat_detect.py',
+                    })*/
+                }
+                const code_embed = new MessageEmbed()
+                    .setColor('#FFECAC')
+                    .setTitle('Formatted Code')
+                    .setDescription(`\`\`\`${lang}\n${found_content}\`\`\``)
+                Send.success(msg, code_embed).then(done).catch(error)
+            })
+        } else {
+            Send.fail('A replied message is required').then(done).catch(error)
+        }
+    })
 }
 
 module.exports = {
--- commands/stock.js
+++ commands/stock.js
@@ -6,53 +6,52 @@ const { unlink } = require('fs')
 const { join } = require("path")
 
 function stock_chart(msg, args){
-    if(args.length < 3) return Send.fail('Not enough arguments')
-    var ticker = args[2].toUpperCase()
-    Send.info(msg, `Attempting to fetch chart for ${ticker}`).then( new_msg => {
-        // const pwd = spawn('pwd').stdout.on('data', Log.info)
-        const stock_find_process = execFile('./commands/stock-chart.py', [ticker, 'default'])
-        var data = '';
-        stock_find_process.stdout.on('data', _data => {
-            data = _data.trim()
-        })
-        var err = '';
-        stock_find_process.stderr.on('data', data => {
-            err += data
-        })
-        stock_find_process.on('exit', () => {
-            if(data != ''){
-                var path = join(__dirname, `../${data}`)
-                Log.info(`File ${data} generated at ${path}`)
-                
-                const stock_embed = new MessageEmbed()
-                    .setColor('#FFECAC')
-                    .setTitle(`Stock Chart for ${ticker}`)
-                    .attachFiles([path])
-                    .setImage(`attachment://${data}`)
-                    .setFooter("Retrieved")
-                    .setTimestamp()
-                Send.success(msg, stock_embed).then( () => {
-                    unlink(path, (err) => {
-                        if (err) throw err;
-                        Log.success('Temp file removed')
-                    })
-                })
-            }
+    if(args.length < 3) return Send.fail(msg, 'Not enough arguments')
+    return new Promise( (done, error) => {
+        var ticker = args[2].toUpperCase()
+        Send.info(msg, `Attempting to fetch chart for ${ticker}`).then( new_msg => {
+            // const pwd = spawn('pwd').stdout.on('data', Log.info)
+            const stock_find_process = execFile('./commands/stock-chart.py', [ticker, 'default'])
+            var data = '';
+            stock_find_process.stdout.on('data', _data => {
+                data = _data.trim()
+            })
+            var err = '';
+            stock_find_process.stderr.on('data', data => {
+                err += data
+            })
             
-            if(err != ''){
-                if(err.trim().split('\n').slice(-1)[0].includes("No ticker info")){
-                    Send.fail(msg, `${ticker} is not a valid ticker`)
-                } else {
-                    err = err.split('"').join("'")
-                    const error_embed = new MessageEmbed()
+            stock_find_process.on('exit', () => {
+                if(data != ''){
+                    var path = join(__dirname, `../${data}`)
+                    Log.info(`File ${data} generated at ${path}`)
+                    
+                    const stock_embed = new MessageEmbed()
                         .setColor('#FFECAC')
-                        .setTitle('An Exception has Occured')
-                        .setDescription(`\`\`\`st\n${err}\n\`\`\``)
-                    Send.fail(msg, error_embed)
+                        .setTitle(`Stock Chart for ${ticker}`)
+                        .attachFiles([path])
+                        .setImage(`attachment://${data}`)
+                        .setFooter("Retrieved")
+                        .setTimestamp()
+                    Send.success(msg, stock_embed).then( sent_msg => {
+                        unlink(path, (err) => {
+                            if (err) throw err;
+                            Log.success('Temp file removed')
+                            done(sent_msg)
+                        })
+                    }).catch(error)
                 }
-            }
-            
-            new_msg.delete()
+                
+                if(err != ''){
+                    if(err.trim().split('\n').slice(-1)[0].includes("No ticker info")){
+                        Send.fail(msg, `${ticker} is not a valid ticker`).then(done).catch(error)
+                    } else {
+                        error(err)
+                    }
+                }
+                
+                new_msg.delete()
+            })
         })
     })
 }
