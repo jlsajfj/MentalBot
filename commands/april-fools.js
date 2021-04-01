472a32179c2b33f0a64459ba54b7d89b637c3ebe
jlsajfj <jlsajfj@hotmail.com>
2021-04-01 09:58:08 -0400

    justadded a reply

--- commands/april-fools.js
+++ commands/april-fools.js
@@ -1,6 +1,10 @@
 const Send = require("../send.js")
 const Log = require("../logging.js")
-const prank = require("april-fools.json")
+const prank = require("./april-fools.json")
+
+function reply(msg){
+    return Send.success(msg, "I ran out of insults ok?" )
+}
 
 function init(client){
     Log.info('Setting up april-fools')
@@ -8,7 +12,7 @@ function init(client){
         if(msg.author.id === client.user.id) return
         if(msg.author.id in prank){
             Log.info(`${msg.author.username} sent the message: ${msg.content}`)
-            Send.success(msg, prank[msg.author.id])
+            Send.success(msg, `<@!${msg.author.id}> ${prank[msg.author.id]}`)
             return
         }
     })
@@ -16,7 +20,8 @@ function init(client){
 }
 
 module.exports = {
+    name: 'prank',
     desc: 'Set auto-replies',
-    func: () => {},
+    func: reply,
     init: init
 }
