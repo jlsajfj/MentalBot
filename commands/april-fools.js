38495f6dff2b3b2cf96fb92a6c604ca71c58f73b
jlsajfj <jlsajfj@hotmail.com>
2021-04-01 11:40:53 -0400

    added auto clearing after 5 seconds

--- commands/april-fools.js
+++ commands/april-fools.js
@@ -12,7 +12,12 @@ function init(client){
         if(msg.author.id === client.user.id) return
         if(msg.author.id in prank){
             Log.info(`${msg.author.username} sent the message: ${msg.content}`)
-            Send.success(msg, `<@!${msg.author.id}> ${prank[msg.author.id]}`)
+            Send.success(msg, `<@!${msg.author.id}> ${prank[msg.author.id]}`).then( sent_msg => {
+                setTimeout(() => {
+                    sent_msg.delete()
+                    Log.success('Message erased')
+                }, 2000)
+            }).catch(Log.fail)
             return
         }
     })
